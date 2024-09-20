29d4d4fee839681ec638290847b4f0a2830d1efe
ljh468 <ljh468@naver.com>
2024-09-20 16:32:44 +0900

    proxy design pattern practice

---
+++ head_first_design/src/main/java/com/design/pattern/proxy/gumball/State.java
@@ -0,0 +1,13 @@
+package com.design.pattern.proxy.gumball;
+
+import java.io.Serializable;
+
+public interface State extends Serializable {
+  public void insertQuarter();
+
+  public void ejectQuarter();
+
+  public void turnCrank();
+
+  public void dispense();
+}
